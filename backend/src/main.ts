c6f6426339fc061ddf579878de5e37159210e26a
renn <renatodsantosjr9@gmail.com>
2024-09-23 15:51:16 +0800

    "bruh"

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
     credentials: true, // If you need to include cookies or auth headers
   });
 
-  await app.listen((process.env.PORT, '0.0.0.0' )|| 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
